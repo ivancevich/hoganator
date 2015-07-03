3bb5a20aa2fef194b0fd85e1e296fe4a2531276b
JC Ivancevich <jc.ivancevich@gmail.com>
2015-07-03 18:54:55 -0300

    partials support

--- test/index.js
+++ test/index.js
@@ -1,12 +1,23 @@
 'use strict';
 var template = require('./output/template');
+var path = require('path');
+var main = require('./output/main');
 var expect = require('chai').expect;
 
 describe('hoganator', function () {
 
   it('should render template', function () {
-    var result = template({ name: 'JC' });
+    var result = template({
+      name: 'JC'
+    });
     expect(result).to.equal('<h1>Hello JC</h1>');
+  });
+
+  it('should render template with partial', function () {
+    var result = main({
+      name: 'JC'
+    });
+    expect(result).to.equal('<section><h1>Hello JC</h1></section>');
   });
 
 });
